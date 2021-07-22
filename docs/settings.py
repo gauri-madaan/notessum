b205b30badf1a7fe56a9b310997be5868638582c
Arush Sharma <arushsharma91@gmail.com>
2021-07-22 15:52:00 +0530

    Update settings.py

--- docs/settings.py
+++ docs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@5n&+p^q*k+&pjtj=+l@o5pj#fy)mslf0_)pfvx-ykl(h)i!3-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['notes-summarizer.herokuapp.com', '127.0.0.1']
 
